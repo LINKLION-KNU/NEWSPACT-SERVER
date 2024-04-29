4a08916e027b4c53bd7ee60a8f3d097c7d168ccb
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-04-29 23:46:12 +0900

    ✨FEAT : Add Entity

---
+++ src/main/java/org/likelion/newsfactbackend/news/domain/News.java
@@ -0,0 +1,40 @@
+package org.likelion.newsfactbackend.news.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.likelion.newsfactbackend.global.domain.BaseTimeEntity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table
+public class News extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String company;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false, unique = true)
+    private String img;
+
+    @Column(nullable = false, unique = true)
+    private String content;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false, unique = true)
+    private String writerEmail;
+
+    @Column(nullable = false)
+    private String date;
+}
