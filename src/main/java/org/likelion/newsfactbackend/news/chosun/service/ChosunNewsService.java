ea8b56dbbcaa40f7197ae60bbed396fcf7431f83
ggowater112@naver.com <127701730+haennni@users.noreply.github.com>
2024-05-13 18:36:58 +0900

    ✨ create controller

--- src/main/java/org/likelion/newsfactbackend/news/chosun/service/ChosunNewsService.java
+++ src/main/java/org/likelion/newsfactbackend/news/chosun/service/ChosunNewsService.java
@@ -1,5 +1,6 @@
 package org.likelion.newsfactbackend.news.chosun.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.likelion.newsfactbackend.news.chosun.dto.request.ChosunNewsRequestDto;
 import org.likelion.newsfactbackend.news.chosun.dto.response.ChosunNewsResponseDto;
 
