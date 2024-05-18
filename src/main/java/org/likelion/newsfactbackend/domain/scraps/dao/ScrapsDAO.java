d1ed4bb05f2bc38202ec7881f66c5f31e6dd05aa
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-05-18 17:12:24 +0900

    ✨ create scrapsDAO interface method
    scraps 조회 DAO 메서드 추가 하였습니다.

--- src/main/java/org/likelion/newsfactbackend/domain/scraps/dao/ScrapsDAO.java
+++ src/main/java/org/likelion/newsfactbackend/domain/scraps/dao/ScrapsDAO.java
@@ -1,8 +1,11 @@
 package org.likelion.newsfactbackend.domain.scraps.dao;
 
 import org.likelion.newsfactbackend.domain.scraps.dto.request.RequestSaveScrapsDto;
+import org.likelion.newsfactbackend.domain.scraps.dto.request.RequestScrapsNewsDto;
+import org.likelion.newsfactbackend.domain.scraps.dto.response.ResponseScrapsNewsDto;
 import org.springframework.http.ResponseEntity;
 
 public interface ScrapsDAO {
     ResponseEntity<?> saveNews(RequestSaveScrapsDto requestSaveScrapsDto);
+    ResponseScrapsNewsDto findScrapsNews(RequestScrapsNewsDto requestScrapsNewsDto);
 }
--- src/main/java/org/likelion/newsfactbackend/domain/scraps/dao/impl/ScrapsDAOImpl.java
+++ src/main/java/org/likelion/newsfactbackend/domain/scraps/dao/impl/ScrapsDAOImpl.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.likelion.newsfactbackend.domain.scraps.dao.ScrapsDAO;
 import org.likelion.newsfactbackend.domain.scraps.dto.request.RequestSaveScrapsDto;
+import org.likelion.newsfactbackend.domain.scraps.dto.request.RequestScrapsNewsDto;
+import org.likelion.newsfactbackend.domain.scraps.dto.response.ResponseScrapsNewsDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,11 @@ import org.springframework.stereotype.Component;
 public class ScrapsDAOImpl implements ScrapsDAO {
     @Override
     public ResponseEntity<?> saveNews(RequestSaveScrapsDto requestSaveScrapsDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseScrapsNewsDto findScrapsNews(RequestScrapsNewsDto requestScrapsNewsDto) {
         return null;
     }
 }
