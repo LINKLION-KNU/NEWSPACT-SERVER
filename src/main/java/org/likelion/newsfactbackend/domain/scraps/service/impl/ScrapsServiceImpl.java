25b0ba2b16d08683a06a3ff880c845ca91bbf864
ggowater112@naver.com <127701730+haennni@users.noreply.github.com>
2024-06-08 19:18:24 +0900

    ✨ create controller
    - News, Scraps, Keywords Controller 생성
    - ScrapsService -> scrapNews 반환 값 및 매개변수 수정, findScrapsById 생성, deletesScrap 생성, getScrapsByPage(페이징 처리) 생성
    - ScrapsServiceImpl -> ScrapsService 구현체 생성

--- src/main/java/org/likelion/newsfactbackend/domain/scraps/service/impl/ScrapsServiceImpl.java
+++ src/main/java/org/likelion/newsfactbackend/domain/scraps/service/impl/ScrapsServiceImpl.java
@@ -2,10 +2,13 @@ package org.likelion.newsfactbackend.domain.scraps.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.likelion.newsfactbackend.domain.scraps.dto.request.RequestSaveScrapsDto;
 import org.likelion.newsfactbackend.domain.scraps.dto.request.RequestScrapsNewsDto;
 import org.likelion.newsfactbackend.domain.scraps.dto.response.ResponseScrapsNewsDto;
 import org.likelion.newsfactbackend.domain.scraps.service.ScrapsService;
 import org.likelion.newsfactbackend.global.domain.CommonResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 @Slf4j
@@ -19,7 +22,21 @@ public class ScrapsServiceImpl implements ScrapsService {
     }
 
     @Override
-    public ResponseEntity<?> scrapNews(RequestScrapsNewsDto requestScrapsNewsDto) {
+    public Boolean findScrapsById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Boolean scrapNews(RequestSaveScrapsDto requestSaveScrapsDto) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteScrap(Long id) {
+        return null;
+    }
+
+    public Page<ResponseScrapsNewsDto> getScrapsByPage(Long id, Pageable pageable){
         return null;
     }
 }
