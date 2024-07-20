041759870263c6400ed07da26af79465cc473ae3
김민재 <redvelvet0625@gmail.com>
2024-07-20 18:02:51 +0900

    :sparkle: add news crawling
    ✨ create news crawling

--- src/main/java/org/likelion/newsfactbackend/domain/news/service/NewsService.java
+++ src/main/java/org/likelion/newsfactbackend/domain/news/service/NewsService.java
@@ -2,9 +2,16 @@ package org.likelion.newsfactbackend.domain.news.service;
 
 import org.likelion.newsfactbackend.domain.news.dto.request.RequestNewsDto;
 import org.likelion.newsfactbackend.domain.news.dto.response.ResponseNewsDto;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.io.IOException;
+import java.util.List;
+
+
+
 public interface NewsService {
-    ResponseNewsDto getNews(RequestNewsDto chosunNewsRequestDto);
+    List<ResponseNewsDto> fetchAllNewsArticles(String search) throws IOException;
+    List<ResponseNewsDto> fetchNewsArticles(String search) throws IOException;
+    Page<ResponseNewsDto> searchNews(RequestNewsDto requestNewsDto) throws IOException;
 }
