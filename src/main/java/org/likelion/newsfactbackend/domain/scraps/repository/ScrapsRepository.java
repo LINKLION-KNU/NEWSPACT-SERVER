5006d9a4c736c95a947ddc5e626bac4d6cb044a3
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-07-30 03:34:24 +0900

    :sparkles: Add scraps function
    ## 개요
    - 뉴스 스크랩 페이지 기능 개발
    - 뉴스 스크랩 기능
    - 스크랩 뉴스 조회 페이징 처리 기능
    - 스크랩 뉴스 삭제 기능
    
    close #42
    
    ## PR 유형
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [x] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [ ] 코드 리팩토링
    - [x] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## PR Checklist
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 오류가 없나요?
    - [x] 클린 코드를 작성했나요?
    - [x] 개념있는 코드를 작성했나요?

--- src/main/java/org/likelion/newsfactbackend/domain/scraps/repository/ScrapsRepository.java
+++ src/main/java/org/likelion/newsfactbackend/domain/scraps/repository/ScrapsRepository.java
@@ -1,7 +1,12 @@
 package org.likelion.newsfactbackend.domain.scraps.repository;
 
 import org.likelion.newsfactbackend.domain.scraps.domain.Scraps;
+import org.likelion.newsfactbackend.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScrapsRepository extends JpaRepository<Scraps, Long> {
+    Page<Scraps> findByUser(User user, Pageable pageable);
+    Scraps findByIdAndUser(Long id, User user);
 }
--- src/main/java/org/likelion/newsfactbackend/global/domain/enums/ResultCode.java
+++ src/main/java/org/likelion/newsfactbackend/global/domain/enums/ResultCode.java
@@ -1,6 +1,7 @@
 package org.likelion.newsfactbackend.global.domain.enums;
 
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 public enum ResultCode {
@@ -8,6 +9,7 @@ public enum ResultCode {
     FAIL(400,"실패"),
     UNAUTHORIZED(403, "권한 없음"),
     PASSWORD_NOT_MATCH(403, "비밀번호 불일치"),
+    TOKEN_IS_NULL(401,"토큰값이 null 입니다."),
     DELETED_USER(401, "탈퇴 유저"),
     EXPIRED_TOKEN(401, "토큰 유효 기간 만료"),
     DUPLICATION_NICKNAME(400, "이미 존재하는 닉네임입니다."),
@@ -22,12 +24,7 @@ public enum ResultCode {
         this.message = message;
     }
 
-    public static ResultCode resolve(int code) {
-        for (ResultCode result : values()) {
-            if (result.getCode() == code) {
-                return result;
-            }
-        }
-        return null;
+    public ResponseEntity<String> toResponseEntity(){
+        return ResponseEntity.status(this.code).body(this.message);
     }
 }
